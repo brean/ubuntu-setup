739a0650d314fb77181f4e99e75d2e35fee45111
Andreas Bresser <self@andreasbresser.de>
2021-05-12 12:29:05 +0200

    add terminator to dev tool install

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # development / ops
-sudo apt install -y make cmake g++ gcc git gitk
+sudo apt install -y make cmake g++ gcc git gitk terminator
 
 # sudo apt install -y openjdk-8-jdk 
 # sudo snap install intellij-idea-community
