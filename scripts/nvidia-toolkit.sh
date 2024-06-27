98b0a4458ea63a60f538d444d0989cea53cfc47c
Andreas Bresser <self@andreasbresser.de>
2024-06-27 20:55:25 +0200

    install nvidia-container-toolkit instead of nvidia-docker2

--- scripts/nvidia-toolkit.sh
+++ scripts/nvidia-toolkit.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
 # based on https://docs.nvidia.com/datacenter/cloud-native/container-toolkit/install-guide.html
+curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
+  && curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | \
+    sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
+    sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
 
-curl https://get.docker.com | sh \
-  && sudo systemctl --now enable docker
-  
-distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
-   && curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add - \
-   && curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
-   
 sudo apt-get update
 
-sudo apt-get install -y nvidia-docker2
+sudo apt-get install -y nvidia-container-toolkit
+
+sudo nvidia-ctk runtime configure --runtime=docker
 
 sudo systemctl restart docker
-
-# test using: sudo docker run --rm --gpus all nvidia/cuda:11.0-base nvidia-smi
-
